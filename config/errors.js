f56a08b439989d26f0889618618498adc8098401
danielfcollier <danielfcollier@gmail.com>
2022-08-13 10:15:29 -0300

    Add receipts scope and fix messages.

--- config/errors.js
+++ config/errors.js
@@ -1,17 +1,24 @@
+const AsaasAPIError = { name: 'AsaasAPIError' };
+
 const ERRORS = {
   CUSTOMER: {
-    CREATE: (customer) => new Error(`Could not create customer! ${customer?.name} - ${customer?.documentId}`, { name: 'AsaasAPIError' }),
-    UPDATE: (customer) => new Error(`Could not update customer! ${customer?.name} - ${customer?.documentId}`, { name: 'AsaasAPIError' }),
-    DELETE: (customer) => new Error(`Could not delete customer! ${customer?.name} - ${customer?.documentId}`, { name: 'AsaasAPIError' }),
+    CREATE: (customer) => new Error(`Could not create customer! ${customer?.name} - ${customer?.documentId}`, AsaasAPIError),
+    UPDATE: (customer) => new Error(`Could not update customer! ${customer?.name} - ${customer?.documentId}`, AsaasAPIError),
+    DELETE: (customer) => new Error(`Could not delete customer! ${customer?.name} - ${customer?.documentId}`, AsaasAPIError),
   },
   PAYMENT: {
-    CREATE: (payment) => new Error(`Could not create payment! ${payment?.id} - ${payment?.description}`, { name: 'AsaasAPIError' }),
-    UPDATE: (payment) => new Error(`Could not update payment! ${payment?.id} - ${payment?.description}`, { name: 'AsaasAPIError' }),
-    DELETE: (payment) => new Error(`Could not delete payment! ${payment?.id} - ${payment?.description}`, { name: 'AsaasAPIError' }),
+    CREATE: (payment) => new Error(`Could not create payment! ${payment?.id} - ${payment?.description}`, AsaasAPIError),
+    UPDATE: (payment) => new Error(`Could not update payment! ${payment?.id} - ${payment?.description}`, AsaasAPIError),
+    DELETE: (payment) => new Error(`Could not delete payment! ${payment?.id} - ${payment?.description}`, AsaasAPIError),
   },
   INSTALLMENT: {
-    CREATE: (installment) => new Error(`Could not create installment! ${installment?.installmentId} - ${installment?.description}`, { name: 'AsaasAPIError' }),
-    UPDATE: (installment) => new Error(`Could not update installment! ${installment?.installmentId} - ${installment?.description}`, { name: 'AsaasAPIError' }),
-    DELETE: (installment) => new Error(`Could not delete installment! ${installment?.installmentId} - ${installment?.description}`, { name: 'AsaasAPIError' }),
+    CREATE: (payment) => new Error(`Could not create installment! Payment: ${payment?.id} - ${payment?.description}`, AsaasAPIError),
+    UPDATE: (payment) => new Error(`Could not update installment! Payment: ${payment?.id} - ${payment?.description}`, AsaasAPIError),
+    DELETE: (payment) => new Error(`Could not delete installment! Payment: ${payment?.id} - ${payment?.description}`, AsaasAPIError),
+  },
+  RECEIPT: {
+    CREATE: (payment) => new Error(`Could not create receipt! Payment: ${payment?.id} - ${payment?.description}`, AsaasAPIError),
+    UPDATE: (payment) => new Error(`Could not update receipt! Payment: ${payment?.id} - ${payment?.description}`, AsaasAPIError),
+    DELETE: (payment) => new Error(`Could not delete receipt! Payment: ${payment?.id} - ${payment?.description}`, AsaasAPIError),
   },
 };
